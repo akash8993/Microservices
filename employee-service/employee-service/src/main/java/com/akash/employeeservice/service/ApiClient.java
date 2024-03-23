5dab38b7bea502f7df6546940afbe75e56583121
Akash Gupta <gr8akash1994@gmail.com>
2024-03-23 18:20:44 +0530

    Changes for Discovery Server and Eureka Client

--- employee-service/employee-service/src/main/java/com/akash/employeeservice/service/ApiClient.java
+++ employee-service/employee-service/src/main/java/com/akash/employeeservice/service/ApiClient.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //Here openfeign client will directly create the implementation of this
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+
+//Now here we have harcoded the url for the feign client and now we will use the name
+// provided by eureka server
+//@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface ApiClient {
 
     @GetMapping("api/departments/getDepartment/{departmentCode}")
