4ca7148589c65680f6774d47906a07d6ff39f3cb
Akash Gupta <gr8akash1994@gmail.com>
2024-03-23 22:37:16 +0530

    Config Server

--- config-server/config-server/src/main/java/com/akash/configserver/ConfigServerApplication.java
+++ config-server/config-server/src/main/java/com/akash/configserver/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package com.akash.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+//With this annotation it will make the whole project to act as a config server
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
